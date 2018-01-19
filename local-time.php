a391066250817d063897574c6b04de1aaff1bd67
Michael Espiritu <michaelgeraldespiritu@gmail.com>
2018-01-19 23:31:01 +0800

    localtime commit

--- local-time.php
+++ local-time.php
@@ -16,7 +16,6 @@ if( !defined('ABSPATH') ){
 }
 
 
-<<<<<<< HEAD
 if ( ! defined( 'LOCALTIME_DIR' ) ) {
 
 	define( 'LOCALTIME_DIR', dirname( __FILE__ ) );
@@ -27,14 +26,3 @@ if ( ! defined( 'LOCALTIME_DIR' ) ) {
 require_once LOCALTIME_DIR . '/public/get.time.php';
 require_once LOCALTIME_DIR . '/admin/get.time-admin-bar.php';
 require_once LOCALTIME_DIR . '/admin/get.time-enqueue.php';
-=======
-require_once( plugin_dir_path( __FILE__ ) . 'get.time.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'get.time-admin-bar.php' );
-
-function local_time_script(){
-    wp_enqueue_script( 'local-time', plugin_dir_url( __FILE__ ) . 'js/local-time.js', array('jquery'), '1.0.0', false );
-}
-
-add_action('wp_enqueue_scripts', 'local_time_script');
-add_action('admin_enqueue_scripts', 'local_time_script');
->>>>>>> 17d2e969eb77355e7004c6a86da8664f58c39f62
