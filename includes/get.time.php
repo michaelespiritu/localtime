8ec5b31972e7c1b46b3161cd3a646b0065f41a7a
Michael Espiritu <michaelgeraldespiritu@gmail.com>
2018-01-26 20:46:19 +0800

    added additional support for shortcode attribute

--- includes/get.time.php
+++ includes/get.time.php
@@ -12,9 +12,37 @@ class LocalTimeShortCode {
 
 		$atts = shortcode_atts( array(
 			'text' => 'Local Time is ',
+			'color' => '',
+			'size' => ''
 		), $atts );
 
-	  $display = '<div class="localtime"><p>'.$atts['text'].' <span id="localtimedisplay"></span></p></div>';
+		$style_array = array();
+		$style = '';
+
+		if( !empty( $atts['color'] ) ) {
+			$style_array['color'] = $atts['color'];
+		}
+
+		if( !empty( $atts['size'] ) )   {
+			$style_array['font-size'] = $atts['size'];
+		}
+
+
+		if (!empty( $style_array ) ){
+
+			$style = 'style="';
+
+			foreach ($style_array as $key => $value) {
+
+					$style .= $key.': '.$value.'; ';
+
+			}
+
+			$style .= '"';
+
+		}
+
+	  $display = '<div class="localtime"><p '. $style .'>'.$atts['text'].' <span id="localtimedisplay"></span></p></div>';
 
 	  return $display;
 
